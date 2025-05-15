4787d2363e78c0d6a2ca58fbe34d5cad2200cc4f
MadHatter965 <madhatter96532@gmail.com>
2025-05-15 07:32:11 -0500

    Added copy static and markdown block to html

--- src/main.py
+++ src/main.py
@@ -1,15 +1,20 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+from copystatic import copy_files_recursive
+
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+
 
 def main():
-    # sample TextNode
-    node = TextNode(
-        "TESTING MY CODE",
-        TextType.LINK,
-        "https://www.website.com"
-    )
-    
-    # Print the node to test the __repr__ method
-    print(node)
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
 
 # Call the main function when the script is run
 if __name__ == "__main__":
